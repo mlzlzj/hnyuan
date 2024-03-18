83ab3c056b0e7d708c989a29c35d25aaeafb7d3b
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-03-18 16:23:04 +0800

    Add files via upload

--- gangaotai.py
+++ gangaotai.py
@@ -1,13 +1,12 @@
 import os
 import requests
-from datetime import datetime
 
 #  获取远程直播源文件
-json_url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt"
-response = requests.get(json_url, timeout=10)
-open('DIYP-v4.txt', 'wb').write(response.content)
+url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt"
+r = requests.get(url)
+open('DIYP-v4.txt', 'wb').write(r.content)
 
-# 打开读取文件和输出文件
+# 打开要读取文件和需要输出的文件
 input_filename = 'DIYP-v4.txt'
 output_filename_hong_kong = 'hong_kong_channels.txt'
 output_filename_taiwan = 'taiwan_channels.txt'
@@ -18,15 +17,15 @@ with open(input_filename, 'r', encoding='utf-8') as infile, \
     out_hk.write('港澳频道,#genre#\n')
     out_tw.write('台湾频道,#genre#\n')
 
-    # 逐行读取文件内容
+    # 逐行读取需要提取包含关键字的内容
     for line in infile:
         if "凤凰卫视" in line or "凤凰资讯" in line or "TVB翡翠" in line or "TVB明珠" in line or "TVB星河" in line \
                 or "J2" in line or "无线" in line or "有线" in line or "天映" in line or "VIU" in line \
-                or "RTHK" in line or "HOY" in line:
+                or "RTHK" in line or "HOY" in line or "香港卫视" in line or "澳亚" in line or "澳视" in line:
             out_hk.write(line)  # 写入香港频道文件
-        if "民视" in line or "中视" in line or "台视" in line or "华视" in line or "新闻台" in line or "TVBS" in line \
-                or "东森" in line or "龙祥" in line or "龙华" in line or "三立" in line or "大爱" in line \
-                or "年代" in line or "人间卫视" in line or "大立" in line or "环球" in line or "亚洲" in line:
+        if "民视" in line or "中视" in line or "台视" in line or "华视" in line or "新闻台" in line \
+                or "东森" in line or "龙祥" in line or "公视" in line or "三立" in line or "大爱" in line \
+                or "年代" in line or "人间卫视" in line or "人間" in line or "大立" in line:
             out_tw.write(line)  # 写入台湾频道文件
 
 # 合并香港频道和台湾频道文件
@@ -41,12 +40,8 @@ for file_path in file_paths:
 with open("gangaotai.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
 
-    # 写入文件生成日期时间
-    now = datetime.now()
-    output.write(f"更新时间,{now.strftime("%Y-%m-%d %H:%M:%S")}")
-
 os.remove("DIYP-v4.txt")
 os.remove("hong_kong_channels.txt")
 os.remove("taiwan_channels.txt")
 
-print("港澳台频道文件gangaotai.txt提取完成！")
+print("港澳台频道文件gangaotai.txt生成完毕！")
