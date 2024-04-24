0ff497404397a3614782cf2065ec2fbf73d57679
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-04-24 08:42:43 +0800

    Update hnyuan.py

--- hnyuan.py
+++ hnyuan.py
@@ -346,10 +346,10 @@ result_counter = 10  # 每个频道需要的个数
 
 with open("iptvlist.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
-    file.write('央视频道,#genre#\n')
+    file.write('央卫频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if 'CCTV' in channel_name:
+        if 'CCTV' in channel_name or '卫视' in channel_name or '凤凰' in channel_name or '翡翠' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
@@ -360,20 +360,20 @@ with open("iptvlist.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
 
-    channel_counters = {}
-    file.write('卫视频道,#genre#\n')
-    for result in results:
-        channel_name, channel_url, speed = result
-        if '卫视' in channel_name or '凤凰' in channel_name or '翡翠' in channel_name:
-            if channel_name in channel_counters:
-                if channel_counters[channel_name] >= result_counter:
-                    continue
-                else:
-                    file.write(f"{channel_name},{channel_url}\n")
-                    channel_counters[channel_name] += 1
-            else:
-                file.write(f"{channel_name},{channel_url}\n")
-                channel_counters[channel_name] = 1
+    # channel_counters = {}
+    # file.write('卫视频道,#genre#\n')
+    # for result in results:
+    #     channel_name, channel_url, speed = result
+    #     if '卫视' in channel_name or '凤凰' in channel_name or '翡翠' in channel_name:
+    #         if channel_name in channel_counters:
+    #             if channel_counters[channel_name] >= result_counter:
+    #                 continue
+    #             else:
+    #                 file.write(f"{channel_name},{channel_url}\n")
+    #                 channel_counters[channel_name] += 1
+    #         else:
+    #             file.write(f"{channel_name},{channel_url}\n")
+    #             channel_counters[channel_name] = 1
 
     channel_counters = {}
     file.write('湖南频道,#genre#\n')
