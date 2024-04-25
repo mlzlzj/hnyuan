7cf4ad4377582d3e0f73b755af9aa2a6093dec67
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-04-25 15:00:48 +0800

    Update hnyuan.py

--- hnyuan.py
+++ hnyuan.py
@@ -1,13 +1,15 @@
 import time
-import os
 import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 import re
-from datetime import datetime
+import os
 import threading
 from queue import Queue
+from datetime import datetime
+import eventlet
+eventlet.monkey_patch()
 
 urls = [
     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iZ3Vhbmdkb25nIg%3D%3D",    # 广 东
@@ -48,7 +50,6 @@ urls = [
     "https://www.zoomeye.org/searchResult?q=city%3A%22shaoyang%22",  # 邵 阳
     "https://www.zoomeye.org/searchResult?q=city%3A%22zhangjiajie%22",  # 张家界
 ]
-
 
 def modify_urls(url):
     modified_urls = []
@@ -156,15 +157,14 @@ for url in urls:
                         name = item.get('name')
                         urlx = item.get('url')
                         if ',' in urlx:
-                            urlx = f"aaaaaaaa"
-
-                        # if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
+                            urlx=f"aaaaaaaa"
+                        #if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
                         if 'http' in urlx:
                             urld = f"{urlx}"
                         else:
                             urld = f"{url_x}{urlx}"
 
-                        if name and urld:
+                        if name and urlx:
                             # 删除特定文字
                             name = name.replace("中央", "CCTV")
                             name = name.replace("高清", "")
@@ -241,8 +241,15 @@ for url in urls:
         except:
             continue
 
-results = set(results)  # 去重得到唯一的URL列表
-results = sorted(results)
+
+channels = []
+
+for result in results:
+    line = result.strip()
+    if result:
+        channel_name, channel_url = result.split(',')
+        channels.append((channel_name, channel_url))
+        
 with open("iptv.txt", 'w', encoding='utf-8') as file:
     for result in results:
         file.write(result + "\n")
@@ -251,26 +258,13 @@ with open("iptv.txt", 'w', encoding='utf-8') as file:
 print("频道列表文件iptv.txt获取完成！")
 
 
-import eventlet
-eventlet.monkey_patch()
-
 # 线程安全的队列，用于存储下载任务
 task_queue = Queue()
 
 # 线程安全的列表，用于存储结果
 results = []
 
-channels = []
 error_channels = []
-# 从iptv.txt文件内提取其他频道进行检测并分组
-with open("iptv.txt", 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-    for line in lines:
-        line = line.strip()
-        if line:
-            channel_name, channel_url = line.split(',')
-            if 'genre' not in channel_url:
-                channels.append((channel_name, channel_url))
 
 
 # 定义工作线程函数
@@ -280,7 +274,7 @@ def worker():
         channel_name, channel_url = task_queue.get()
         try:
             channel_url_t = channel_url.rstrip(channel_url.split('/')[-1])  # m3u8链接前缀
-            lines = requests.get(channel_url).text.strip().split('\n')  # 获取m3u8文件内容
+            lines = requests.get(channel_url, timeout = 1).text.strip().split('\n')  # 获取m3u8文件内容
             ts_lists = [line.split('/')[-1] for line in lines if line.startswith('#') == False]  # 获取m3u8文件下视频流后缀
             ts_lists_0 = ts_lists[0].rstrip(ts_lists[0].split('.ts')[-1])  # m3u8链接前缀
             ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
@@ -288,7 +282,7 @@ def worker():
             # 多获取的视频数据进行5秒钟限制
             with eventlet.Timeout(5, False):
                 start_time = time.time()
-                content = requests.get(ts_url).content
+                content = requests.get(ts_url, timeout = 1).content
                 end_time = time.time()
                 response_time = (end_time - start_time) * 1
 
@@ -300,21 +294,19 @@ def worker():
                 download_speed = file_size / response_time / 1024
                 # print(f"下载速度：{download_speed:.3f} kB/s")
                 normalized_speed = min(max(download_speed / 1024, 0.001), 100)  # 将速率从kB/s转换为MB/s并限制在1~100之间
-                # print(f"标准化后的速率：{normalized_speed:.3f} MB/s")
+                #print(f"标准化后的速率：{normalized_speed:.3f} MB/s")
 
                 # 删除下载的文件
                 os.remove(ts_lists_0)
                 result = channel_name, channel_url, f"{normalized_speed:.3f} MB/s"
                 results.append(result)
                 numberx = (len(results) + len(error_channels)) / len(channels) * 100
-                print(
-                    f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+                print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
         except:
             error_channel = channel_name, channel_url
             error_channels.append(error_channel)
             numberx = (len(results) + len(error_channels)) / len(channels) * 100
-            print(
-                f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+            print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
 
         # 标记任务完成
         task_queue.task_done()
@@ -323,10 +315,8 @@ def worker():
 # 创建多个工作线程
 num_threads = 10
 for _ in range(num_threads):
-    t = threading.Thread(target=worker, daemon=True)
-    # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
+    t = threading.Thread(target=worker, daemon=True)  # 将工作线程设置为守护线程
     t.start()
-    # event.set()
 
 # 添加下载任务到队列
 for channel in channels:
@@ -343,11 +333,11 @@ def channel_key(channel_name):
     else:
         return float('inf')  # 返回一个无穷大的数字作为关键字
 
-
 # 对频道进行排序
 results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 results.sort(key=lambda x: channel_key(x[0]))
-result_counter = 10  # 每个频道需要的个数
+
+result_counter = 20  # 每个频道需要的个数
 
 with open("iptvlist.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
@@ -385,6 +375,19 @@ with open("iptvlist.txt", 'w', encoding='utf-8') as file:
     for result in results:
         channel_name, channel_url, speed = result
         if '湖南' in channel_name or '长沙' in channel_name or '金鹰' in channel_name or '先锋乒羽' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+    file.write('其他频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name and '凤凰' not in channel_name and '翡翠' not in channel_name and 'CHC' not in channel_name and '湖南' not in channel_name and '长沙' not in channel_name and '金鹰' not in channel_name and '先锋乒羽' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
