bc26b18d7f5f771449c41d754e237b0840bf7f3d
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-04-26 14:49:52 +0800

    Update hnyuan.py

--- hnyuan.py
+++ hnyuan.py
@@ -11,6 +11,12 @@ from datetime import datetime
 import eventlet
 eventlet.monkey_patch()
 
+# 获取央视频道
+url = 'https://taoiptv.com/source/Yshipin.txt?token=8zlxhttq9h01ahaw'
+r = requests.get(url)
+with open("Yshipin.txt", "wb") as code:
+    code.write(r.content)
+
 urls = [
     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iZ3Vhbmdkb25nIg%3D%3D",    # 广 东
     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ic2hlbnpoZW4i",  # 深 圳
@@ -401,7 +407,7 @@ with open("iptvlist.txt", 'w', encoding='utf-8') as file:
 
 # 合并自定义频道文件内容
 file_contents = []
-file_paths = ["YD-IPTV.txt", "iptvlist.txt", "gangaotai.txt", "zdy.txt"]  # 替换为实际的文件路径列表
+file_paths = ["YD-IPTV.txt", "Yshipin.txt", "iptvlist.txt", "gangaotai.txt", "zdy.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
@@ -421,5 +427,6 @@ with open("iptv_list.txt", "w", encoding="utf-8") as output:
 os.remove("iptv.txt")
 os.remove("iptvlist.txt")
 os.remove("gangaotai.txt")
+os.remove("Yshipin.txt")
 
 print("任务运行完毕，分类频道列表可查看文件夹内hunan.txt文件！")
