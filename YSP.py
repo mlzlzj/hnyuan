145be10e4dd254490784e0ba855e940d28652e0e
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-04-26 15:23:53 +0800

    Delete YSP.py

--- YSP.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# 获取央视频道
-url = 'https://taoiptv.com/source/Yshipin.txt?token=8zlxhttq9h01ahaw'
-r = requests.get(url)
-with open("Yshipin.txt", "wb") as code:
-    code.write(r.content)
