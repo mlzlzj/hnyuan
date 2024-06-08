3bd6dd61805a1e0399ecf12fae84144435be94f0
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-06-09 01:09:59 +0800

    Update hnyuan.py

--- hnyuan.py
+++ hnyuan.py
@@ -15,7 +15,7 @@ import subprocess
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-shengshi_names = ["湖南", "江西", "广东", "山东", "长沙", "娄底", "衡阳", "常德", "南宁"]
+shengshi_names = ["湖南", "山东", "长沙", "娄底", "衡阳", "常德", "南宁"]
 print(f'本次扫描{shengshi_names}的酒店频道。')
 pinyin_names = ["".join(lazy_pinyin(name, errors=lambda x: x)) for name in shengshi_names]
 
